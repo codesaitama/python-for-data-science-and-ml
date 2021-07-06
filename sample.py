4c93029c057668a83b55a83885e2ecb00abe7d52
Emmanuel Achana <achanamosey@gmail.com>
2021-07-06 14:50:28 +0000

    Added examples of [filter], [map], [lambda] use.

--- sample.py
+++ sample.py
@@ -7,6 +7,14 @@ def square(num: int):
     return num ** 2
 
 # Lambda version of the square function
-lambda_square = lambda num : num * 2
+lambda_square = lambda num : num ** 2
 
-print(list(map(square, seq)))
+print(lambda_square(7))
+
+#Using map
+map_example = list(map(square, seq))
+print(map_example)
+
+#Using filter
+filter_example = list(filter(lambda num: num % 2 == 0, seq))
+print(filter_example)
